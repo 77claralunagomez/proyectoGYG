d97ee7c9556007d9c2247331d85298cd5ee71625
77claralunagomez <claragomez968@gmail.com>
2025-06-25 19:32:48 -0300

    arreglado el footer

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -4,7 +4,7 @@
       
       <div class="col-12 col-sm-6 col-md-3 mb-4 d-flex flex-column align-items-center align-items-md-start">
         <h5>
-          <a href="legales" class="btn btn-outline-light btn-sm boton-footer">Legales</a>
+          <a href="<?= base_url('legales'); ?>" class="btn btn-outline-light btn-sm boton-footer">Legales</a>
         </h5>
         <ul class="list-unstyled">
           <li>Términos y condiciones</li>
@@ -15,7 +15,7 @@
 
       <div class="col-12 col-sm-6 col-md-3 mb-4 d-flex flex-column align-items-center align-items-md-start">
         <h5>
-          <a href="empresa" class="btn btn-outline-light btn-sm boton-footer">Empresa</a>
+          <a href="<?= base_url('empresa'); ?>" class="btn btn-outline-light btn-sm boton-footer">Empresa</a>
         </h5>
         <ul class="list-unstyled">
           <li>Sobre nosotros</li>
@@ -25,7 +25,7 @@
 
       <div class="col-12 col-sm-6 col-md-3 mb-4 d-flex flex-column align-items-center align-items-md-start">
         <h5>
-          <a href="ayuda" class="btn btn-outline-light btn-sm boton-footer">Ayuda</a>
+          <a href="<?= base_url('ayuda'); ?>" class="btn btn-outline-light btn-sm boton-footer">Ayuda</a>
         </h5>
         <ul class="list-unstyled">
           <li>Consultas frecuentes</li>
@@ -35,7 +35,7 @@
 
       <div class="col-12 col-sm-6 col-md-3 mb-4 d-flex flex-column align-items-center align-items-md-start">
         <h5>
-          <a href="atencion" class="btn btn-outline-light btn-sm boton-footer">Atención</a>
+          <a href="<?= base_url('atencion'); ?>" class="btn btn-outline-light btn-sm boton-footer">Atención</a>
         </h5>
         <ul class="list-unstyled">
           <li>3794778687</li>
